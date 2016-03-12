f6ca091c3d29caa0a1432816928bfcb2e3e6ac0b
glenn222 <glendon.ngo@gmail.com>
2016-03-12 14:22:24 -0500

    Updated user branch from Portal workspace (server script)

--- serverscript.js
+++ serverscript.js
@@ -17,3 +17,21 @@ function removeLost(){
 }
 function removeFound(){
 }
+function getStudentData(){
+    // Use Student object to retrieve all available student info
+    var studentInfo = {
+		career: user.Student.Career,
+		faculty: user.Student.Faculty,
+		departments: user.Student.Departments,
+		plans: user.Student.PlanTitles,
+		formOfStudy: user.Student.FormOfStudy,
+		level: user.Student.Level,
+		studentNum: user.Student.StudentNumber
+	};
+    
+	// Can log the whole object to check what is being returned
+	console.log(studentInfo);
+	
+	// Return final result	
+    return studentInfo;
+}
