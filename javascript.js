e87c9f2858376f2402ab06ee6653d1f0dd52a63e
tinaq <tina.qian@yahoo.ca>
2016-03-13 05:28:01 +0900

    Updated user branch from Portal workspace (javascript.js)

--- javascript.js
+++ javascript.js
@@ -179,8 +179,7 @@ angular.module('portalApp')
             $scope.portalHelpers.invokeServerFunction('getTable', {table:lostTable})
                 .then(function(result) {
                     $scope.lostTable = result;
-                });
-            
+                });           
             $scope.portalHelpers.invokeServerFunction('insert', {
                 value: $scope.insertValue.value
             }).then(function (result) {
